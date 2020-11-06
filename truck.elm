58a2fdf4721e120070711e330fac5ae0be4a497b
Daniel VonMoser <36549141+DannyV99@users.noreply.github.com>
2020-11-06 08:59:55 -0600

    Update truck.elm

--- truck.elm
+++ truck.elm
@@ -1,35 +1,74 @@
+module Main exposing (changer, main, update, view)
 import Playground exposing (..)
 
-
 main =
-  game view update
+    game view update initModel
+type alias Model =
+    { x : Number, y : Number, angle : Number, garbageCanStatus : GarbageCanStatus }
+type GarbageCanStatus
+    = FullOfGarbage
+    | Emptied
+initModel : Model
+initModel =
     { x = 0
     , y = 0
     , angle = 0
+    , garbageCanStatus = FullOfGarbage
     }
-
-view computer truck =
-
-  [ rectangle blue computer.screen.width computer.screen.height
-  , image 300 300 "images/GarbageTuck.gif"
-      |> move truck.x truck.y
-      |> rotate truck.angle
-
-  , image 90 90 (changer truck 300)
-        |> moveRight 300
-        |> moveDown 20
-  ]
-
-
-changer truck x =
-  if truck.x > x then
-    "images/emptyGarbage" ++ ".jpg"
-  else
-    "images/fullGarbage" ++ ".jpg"
-
-
-update computer truck =
-  { x = truck.x + toY computer.keyboard * cos (degrees truck.angle)
-  , y = truck.y + toY computer.keyboard * sin (degrees truck.angle)
-  , angle = truck.angle - toX computer.keyboard
-  }
+view : Computer -> Model -> List Shape
+view computer model =
+    [ rectangle blue computer.screen.width computer.screen.height
+    , image 300 300 "images/GarbageTruck.gif"
+        |> move model.x model.y
+        |> rotate model.angle
+    , garbageCanView model
+    ]
+garbageCanView model =
+    case model.garbageCanStatus of
+        FullOfGarbage ->
+            image 90 90 "images/fullGarbage.jpg"
+                |> moveRight 300
+                |> moveDown 20
+        Emptied ->
+            image 90 90 ("images/emptyGarbage" ++ ".jpg")
+                |> moveRight 300
+                |> moveDown 20
+update : Computer -> Model -> Model
+update computer model =
+    let
+        updatedX =
+            model.x + toY computer.keyboard * cos (degrees model.angle)
+        updatedY =
+            model.y + toY computer.keyboard * sin (degrees model.angle)
+        updatedAngle =
+            model.angle - toX computer.keyboard
+        updatedGarbageCanStatus =
+            updateGarbageCanStatus model
+    in
+    { model
+        | x = updatedX
+        , y = updatedY
+        , angle = updatedAngle
+        , garbageCanStatus = updatedGarbageCanStatus
+    }
+updateGarbageCanStatus model =
+    case model.garbageCanStatus of
+        Emptied ->
+            Emptied
+        FullOfGarbage ->
+            if changer model 300 20 then
+                Emptied
+            else
+                FullOfGarbage
+changer : Model -> Number -> Number -> Bool
+changer truck x y =
+    let
+        buffer =
+            150
+    in
+    [ (truck.x - buffer) < x
+    , x < (truck.x + buffer)
+    , (truck.y - buffer) < y
+    , y < (truck.y + buffer)
+    ]
+        |> List.all identity
