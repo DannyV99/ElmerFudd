10f99d63d0c5108ff92763b713ac8eefbcc970fa
DannyV99 <dgvonmoser@gmail.com>
2020-11-07 07:04:32 -0600

    Updated Status

--- truck.elm
+++ truck.elm
@@ -4,35 +4,64 @@ import Playground exposing (..)
 main =
     game view update initModel
 type alias Model =
-    { x : Number, y : Number, angle : Number, garbageCanStatus : GarbageCanStatus }
+    { x : Number, y : Number, angle : Number, garbageCan : GarbageCan }
 type GarbageCanStatus
     = FullOfGarbage
     | Emptied
+
+type alias GarbageCan
+  = { x : Number, y : Number, status: GarbageCanStatus }
+
 initModel : Model
 initModel =
     { x = 0
     , y = 0
-    , angle = 0
-    , garbageCanStatus = FullOfGarbage
+    , angle = 25
+    , garbageCan = { x = 600, y = 20, status = FullOfGarbage }
     }
+
+
 view : Computer -> Model -> List Shape
 view computer model =
     [ rectangle blue computer.screen.width computer.screen.height
-    , image 300 300 "images/GarbageTruck.gif"
-        |> move model.x model.y
-        |> rotate model.angle
-    , garbageCanView model
+    , image 1400 1400 "images/Road.png"
+        |> moveRight 1
+        |> moveDown 1
+    , garbageTruckView model
+        |> moveRight 1
+        |> moveDown 200
+    , garbageCanView model.garbageCan.status model.garbageCan.x model.garbageCan.y
     ]
-garbageCanView model =
-    case model.garbageCanStatus of
+
+garbageCanView status x y =
+    case status of
         FullOfGarbage ->
-            image 90 90 "images/fullGarbage.jpg"
-                |> moveRight 300
-                |> moveDown 20
+            image 110 110 "images/fullGarbage.jpg"
+                |> moveRight x
+                |> moveDown y
         Emptied ->
-            image 90 90 ("images/emptyGarbage" ++ ".jpg")
-                |> moveRight 300
-                |> moveDown 20
+            image 110 100 ("images/emptyGarbage.jpg")
+                |> moveRight x
+                |> moveDown y
+
+
+garbageTruckView model =
+        if model.angle > 75 then
+            image 350 350 "images/GarbageTruckLeft0.gif"
+              |> move model.x model.y
+              |> rotate (model.angle - 180)
+        else if model.angle > 90 then
+            image 350 350 "images/GarbageTruckRight0.gif"
+                |> move model.x model.y
+                |> rotate (model.angle - 180)
+
+        else
+            image 350 350 ("images/GarbageTruckRight0.gif")
+                |> move model.x model.y
+                |> rotate model.angle
+
+
+
 update : Computer -> Model -> Model
 update computer model =
     let
@@ -41,25 +70,44 @@ update computer model =
         updatedY =
             model.y + toY computer.keyboard * sin (degrees model.angle)
         updatedAngle =
+          if model.angle > -1 && model.angle < 180  then
             model.angle - toX computer.keyboard
-        updatedGarbageCanStatus =
-            updateGarbageCanStatus model
+          else if model.angle == -1 && computer.keyboard.left then
+            model.angle + 1
+          else if model.angle == 180 && computer.keyboard.right then
+            model.angle - 1
+          else
+            model.angle
+        updatedGarbageCan =
+            updateGarbageCan model
     in
     { model
         | x = updatedX
         , y = updatedY
-        , angle = updatedAngle
-        , garbageCanStatus = updatedGarbageCanStatus
+        , angle = Debug.log "HI" updatedAngle
+        , garbageCan = updatedGarbageCan
     }
-updateGarbageCanStatus model =
-    case model.garbageCanStatus of
-        Emptied ->
-            Emptied
-        FullOfGarbage ->
-            if changer model 300 20 then
+
+
+updateGarbageCan : Model -> GarbageCan
+updateGarbageCan model =
+    let
+        existingGC = model.garbageCan
+        updatedStatus =
+
+          case model.garbageCan.status of
+            Emptied ->
+              Emptied
+            FullOfGarbage ->
+              if changer model model.garbageCan.x model.garbageCan.y then
                 Emptied
-            else
+              else
                 FullOfGarbage
+
+    in
+    { existingGC | status = updatedStatus}
+
+
 changer : Model -> Number -> Number -> Bool
 changer truck x y =
     let
