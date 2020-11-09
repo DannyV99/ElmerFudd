0f0e18eec6b3e056e16e583136524a8691b75c37
DannyV99 <dgvonmoser@gmail.com>
2020-11-09 12:49:52 -0600

    Getting it done

--- truck.elm
+++ truck.elm
@@ -1,24 +1,53 @@
 module Main exposing (changer, main, update, view)
 import Playground exposing (..)
 
+
+
+-- Main --
+
 main =
     game view update initModel
+
+
+-- Types --
+
+
 type alias Model =
-    { x : Number, y : Number, angle : Number, garbageCan : GarbageCan }
+    { x : Number, y : Number, angle : Number, garbageCan : GarbageCan, garbageCan2 : GarbageCan2 }
+
 type GarbageCanStatus
     = FullOfGarbage
     | Emptied
 
+
 type alias GarbageCan
   = { x : Number, y : Number, status: GarbageCanStatus }
+
+type alias GarbageCan2
+  = { x : Number, y : Number, status: GarbageCanStatus }
+
+  -- Model --
 
 initModel : Model
 initModel =
     { x = 0
     , y = 0
-    , angle = 25
-    , garbageCan = { x = 600, y = 20, status = FullOfGarbage }
+    , angle = 0
+    , garbageCan = position1
+    , garbageCan2 = position2
     }
+
+
+position1 =
+
+  { x = 600, y = 20, status = FullOfGarbage }
+
+position2 =
+
+  { x = 300, y = -300, status = FullOfGarbage }
+
+
+-- View --
 
 
 view : Computer -> Model -> List Shape
@@ -31,6 +60,7 @@ view computer model =
         |> moveRight 1
         |> moveDown 200
     , garbageCanView model.garbageCan.status model.garbageCan.x model.garbageCan.y
+    , garbageCanView model.garbageCan2.status model.garbageCan2.x model.garbageCan2.y
     ]
 
 garbageCanView status x y =
@@ -54,7 +84,6 @@ garbageTruckView model =
             image 350 350 "images/GarbageTruckRight0.gif"
                 |> move model.x model.y
                 |> rotate (model.angle - 180)
-
         else
             image 350 350 ("images/GarbageTruckRight0.gif")
                 |> move model.x model.y
@@ -62,13 +91,16 @@ garbageTruckView model =
 
 
 
+-- Update --
+
+
 update : Computer -> Model -> Model
 update computer model =
     let
         updatedX =
-            model.x + toY computer.keyboard * cos (degrees model.angle)
+            model.x + toY computer.keyboard * 4 * cos (degrees model.angle)
         updatedY =
-            model.y + toY computer.keyboard * sin (degrees model.angle)
+            model.y + toY computer.keyboard * 4 * sin (degrees model.angle)
         updatedAngle =
           if model.angle > -1 && model.angle < 180  then
             model.angle - toX computer.keyboard
@@ -80,12 +112,15 @@ update computer model =
             model.angle
         updatedGarbageCan =
             updateGarbageCan model
+        updatedGarbageCan2 =
+            updateGarbageCan2 model
     in
     { model
         | x = updatedX
         , y = updatedY
         , angle = Debug.log "HI" updatedAngle
         , garbageCan = updatedGarbageCan
+        , garbageCan2 = updatedGarbageCan2
     }
 
 
@@ -100,6 +135,25 @@ updateGarbageCan model =
               Emptied
             FullOfGarbage ->
               if changer model model.garbageCan.x model.garbageCan.y then
+                Emptied
+              else
+                FullOfGarbage
+
+    in
+    { existingGC | status = updatedStatus}
+
+
+updateGarbageCan2 : Model -> GarbageCan2
+updateGarbageCan2 model =
+    let
+        existingGC = model.garbageCan2
+        updatedStatus =
+
+          case model.garbageCan2.status of
+            Emptied ->
+              Emptied
+            FullOfGarbage ->
+              if changer model model.garbageCan2.x model.garbageCan2.y then
                 Emptied
               else
                 FullOfGarbage
