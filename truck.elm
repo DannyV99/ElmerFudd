a52601a1af3256da7dc500e3acef15b5b76abab1
DannyV99 <dgvonmoser@gmail.com>
2020-11-12 13:31:43 -0600

    Good spot

--- truck.elm
+++ truck.elm
@@ -1,226 +1,244 @@
 module Main exposing (changer, main, update, view)
-import Playground exposing (..)
 
+import Playground exposing (..)
 
 
 -- Main --
 
+
 main =
     game view update initModel
+
 
 
 -- Types --
 
 
 type alias Model =
-    { x : Number, y : Number, angle : Number, garbageCan : GarbageCan, garbageCan2 : GarbageCan2, level : Int}
+    { x : Number, y : Number, angle : Number, garbageCans : List GarbageCan, levelUp : Shape }
+
 
 type GarbageCanStatus
     = FullOfGarbage
     | Emptied
 
 
-type alias GarbageCan
-  = { x : Number, y : Number, status: GarbageCanStatus }
-
-type alias GarbageCan2
-  = { x : Number, y : Number, status: GarbageCanStatus }
+type alias GarbageCan =
+    { x : Number, y : Number, status : GarbageCanStatus }
 
 
-  -- Model --
+type alias GarbageCan2 =
+    { x : Number, y : Number, status : GarbageCanStatus }
+
+
+
+
+-- Model --
+
 
 initModel : Model
 initModel =
-    { x = 0
-    , y = 0
+    { x = -400
+    , y = -250
     , angle = 0
-    , garbageCan = position1
-    , garbageCan2 = position2
-    , level = 1
+    , garbageCans = initGarbageCans
+    , levelUp = levelUpImage 1 1
     }
 
 
-position1 =
+initGarbageCans =
+    [ position1, position2 ]
 
-  { x = 600, y = 20, status = FullOfGarbage }
+
+position1 =
+    { x = -100, y = 20, status = FullOfGarbage }
+
 
 position2 =
+    { x = 300, y = -300, status = FullOfGarbage }
 
-  { x = 300, y = -300, status = FullOfGarbage }
-
-
+levelUpImage x y =
+  image 110 110 "images/nextLevel.jpg"
+      |> move x y
 
 -- View --
 
 
 view : Computer -> Model -> List Shape
 view computer model =
-  if model.level == 1 then
     [ rectangle blue computer.screen.width computer.screen.height
-    --image 10 10 "images/Road.png"
-        --|> moveRight 1
-        --|> moveDown 1
     , garbageTruckView model
-        |> moveRight 1
-        |> moveDown 200
-    , garbageCanView model.garbageCan.status model.garbageCan.x model.garbageCan.y
-    , rectangle red 2000 10
-        |> move 20 20
     ]
-  else if model.level == 2 then
-    [ rectangle blue computer.screen.width computer.screen.height
-    --image 10 10 "images/Road.png"
-        --|> moveRight 1
-        --|> moveDown 1
-    , garbageTruckView model
-        |> moveRight 1
-        |> moveDown 200
-    , garbageCanView model.garbageCan.status model.garbageCan.x model.garbageCan.y
-    , garbageCanView model.garbageCan2.status model.garbageCan2.x model.garbageCan2.y
-    , rectangle red 2000 10
-        |> move 20 125
-    , rectangle red 2000 10
-        |> move 20 -125
-    ]
-  else
-    [image 1000 1000 "images/GameOver.jpg"
-      |> move 1 1]
+        ++ garbageCansView model.garbageCans
 
 
-garbageCanView status x y =
+garbageCansView : List GarbageCan -> List Shape
+garbageCansView gCs =
+    List.map garbageCanView gCs
+
+
+garbageCanView gC =
+    let
+        status =
+            gC.status
+
+        x =
+            gC.x
+
+        y =
+            gC.y
+    in
     case status of
         FullOfGarbage ->
             image 110 110 "images/fullGarbage.jpg"
-              |> move x y
+                |> move x y
+
         Emptied ->
-            image 110 100 ("images/emptyGarbage.jpg")
-              |> move x y
+            image 110 100 "images/emptyGarbage.jpg"
+                |> move x y
 
 
 garbageTruckView model =
-        if model.angle > 75 then
-            image 350 350 "images/GarbageTruckLeft0.gif"
-              |> move model.x model.y
-              |> rotate (model.angle - 180)
-        else if model.angle > 90 then
-            image 350 350 "images/GarbageTruckRight0.gif"
-                |> move model.x model.y
-                |> rotate (model.angle - 180)
-        else
-            image 350 350 ("images/GarbageTruckRight0.gif")
-                |> move model.x model.y
-                |> rotate model.angle
+    if model.angle > 75 then
+        image 350 350 "images/GarbageTruckLeft0.gif"
+            |> move model.x model.y
+            |> rotate (model.angle - 180)
+
+    else if model.angle > 90 then
+        image 350 350 "images/GarbageTruckRight0.gif"
+            |> move model.x model.y
+            |> rotate (model.angle - 180)
+
+    else
+        image 350 350 "images/GarbageTruckRight0.gif"
+            |> move model.x model.y
+            |> rotate model.angle
 
 
 
 -- Update --
 
 
+isEmpty : GarbageCan -> Bool
+isEmpty gC =
+    case gC.status of
+        FullOfGarbage ->
+            False
+
+        Emptied ->
+            True
+
+
+
+
 update : Computer -> Model -> Model
 update computer model =
-  if model.garbageCan.status == FullOfGarbage then
     let
-        updatedX =
-            model.x + toY computer.keyboard * 4 * cos (degrees model.angle)
-        updatedY =
-            model.y + toY computer.keyboard * 4 * sin (degrees model.angle)
+        advanceToNextLevel =
+            List.all isEmpty model.garbageCans
+
+        ( updatedX, updatedY ) =
+            updateXY computer ( model.x, model.y ) model.angle advanceToNextLevel
+
         updatedAngle =
-          if model.angle > -1 && model.angle < 180  then
-            model.angle - toX computer.keyboard
-          else if model.angle == -1 && computer.keyboard.left then
-            model.angle + 1
-          else if model.angle == 180 && computer.keyboard.right then
-            model.angle - 1
-          else
-            model.angle
-        updatedGarbageCan =
-            updateGarbageCan model
-        updatedGarbageCan2 =
-            updateGarbageCan2 model
+            updateAngle computer model
+
+        updatedGarbageCans =
+            updateGarbageCans ( model.x, model.y ) advanceToNextLevel model.garbageCans model.angle
     in
     { model
         | x = updatedX
         , y = updatedY
-        , angle = Debug.log "HI" updatedAngle
-        , garbageCan = updatedGarbageCan
-        , garbageCan2 = updatedGarbageCan2
-    }
-  else
-    let
-        updatedX =
-            model.x + toY computer.keyboard * cos (degrees model.angle)
-        updatedY =
-            model.y + toY computer.keyboard * sin (degrees model.angle)
-        updatedAngle =
-          if model.angle > -1 && model.angle < 180  then
-            model.angle - toX computer.keyboard
-          else if model.angle == -1 && computer.keyboard.left then
-            model.angle + 1
-          else if model.angle == 180 && computer.keyboard.right then
-            model.angle - 1
-          else
-            model.angle
-        updatedGarbageCan =
-            updateGarbageCan model
-        updatedGarbageCan2 =
-            updateGarbageCan2 model
-    in
-    { model
-        | x = updatedX
-        , y = updatedY
-        , angle = Debug.log "HI" updatedAngle
-        , garbageCan = updatedGarbageCan
-        , garbageCan2 = updatedGarbageCan2
+        , angle = updatedAngle
+        , garbageCans = updatedGarbageCans
     }
 
 
-updateGarbageCan : Model -> GarbageCan
-updateGarbageCan model =
+fill : GarbageCan -> GarbageCan
+fill gC =
+    { gC | status = FullOfGarbage }
+
+
+
+updateGarbageCans : ( Number, Number ) -> Bool -> List GarbageCan -> Number -> List GarbageCan
+updateGarbageCans truckPosition advance gCs angle =
+    if advance then
+        let
+            newGCs =
+                List.map fill gCs
+
+            newCan =
+                case List.head gCs of
+                    Just headGC ->
+                        { status = FullOfGarbage, x = headGC.x - (angle), y = headGC.y + ( 50 ) }
+
+                    Nothing ->
+                        position2
+        in
+        newCan :: newGCs
+
+    else
+        List.map (updateGarbageCan truckPosition) gCs
+
+
+
+
+updateAngle computer model =
+    if model.angle > -1 && model.angle < 180 then
+        model.angle - toX computer.keyboard
+
+    else if model.angle == -1 && computer.keyboard.left then
+        model.angle + 1
+
+    else if model.angle == 180 && computer.keyboard.right then
+        model.angle - 1
+
+    else
+        model.angle
+
+
+updateXY computer ( truckX, truckY ) angle advanceToNextLevel =
+    if advanceToNextLevel then
+        ( -300, -400 )
+
+    else
+        let
+            updatedX =
+                truckX + toY computer.keyboard * 6 * cos (degrees angle)
+
+            updatedY =
+                truckY + toY computer.keyboard * 6 * sin (degrees angle)
+        in
+        ( updatedX, updatedY )
+
+
+updateGarbageCan : ( Number, Number ) -> GarbageCan -> GarbageCan
+updateGarbageCan truckPosition existingGC =
     let
-        existingGC = model.garbageCan
         updatedStatus =
+            case existingGC.status of
+                Emptied ->
+                    Emptied
 
-          case model.garbageCan.status of
-            Emptied ->
-              Emptied
-            FullOfGarbage ->
-              if changer model model.garbageCan.x model.garbageCan.y then
-                Emptied
-              else
-                FullOfGarbage
+                FullOfGarbage ->
+                    if changer truckPosition existingGC.x existingGC.y then
+                        Emptied
 
+                    else
+                        FullOfGarbage
     in
-    { existingGC | status = updatedStatus}
+    { existingGC | status = updatedStatus }
 
 
-updateGarbageCan2 : Model -> GarbageCan2
-updateGarbageCan2 model =
-    let
-        existingGC = model.garbageCan2
-        updatedStatus =
-
-          case model.garbageCan2.status of
-            Emptied ->
-              Emptied
-            FullOfGarbage ->
-              if changer model model.garbageCan2.x model.garbageCan2.y then
-                Emptied
-              else
-                FullOfGarbage
-
-    in
-    { existingGC | status = updatedStatus}
-
-
-changer : Model -> Number -> Number -> Bool
-changer truck x y =
+changer : ( Number, Number ) -> Number -> Number -> Bool
+changer ( truckX, truckY ) x y =
     let
         buffer =
             150
     in
-    [ (truck.x - buffer) < x
-    , x < (truck.x + buffer)
-    , (truck.y - buffer) < y
-    , y < (truck.y + buffer)
+    [ (truckX - buffer) < x
+    , x < (truckX + buffer)
+    , (truckY - buffer) < y
+    , y < (truckY + buffer)
     ]
         |> List.all identity
